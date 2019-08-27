e8d7780d55d3e4fec1a4f347ac8739f6b7fcf69f
itke96 <itan96@live.com>
2019-08-27 22:59:19 +0200

    dodat registercontroller
    dodat je novi kontroler

---
+++ Web2Proj/WebApp/WebApp/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApp.Persistence.UnitOfWork;
+
+namespace WebApp.Controllers
+{
+    [RoutePrefix("api/Registrovanje")]
+    public class RegisterController : ApiController
+    {
+        IUnitOfWork _unitOfWork;
+
+        public RegisterController(IUnitOfWork unitofWork)
+        {
+            _unitOfWork = unitofWork;
+        }
+    }
+}
